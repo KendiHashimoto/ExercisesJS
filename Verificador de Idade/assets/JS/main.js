0e987bbb236f4d0665ce6b6a01333ce764226782
Hashimoto 1312 <kendi.campos@hotmail.com>
2021-10-19 10:11:09 -0300

    Falta as imagens
    Por algum motivo as imagens não estão aparecendo,talvez seja um erro sutil pesquisei muito vou deixar pra ver depois.

--- Verificador de Idade/assets/JS/main.js
+++ Verificador de Idade/assets/JS/main.js
@@ -4,25 +4,40 @@ function verificar () {
     const anoNascimento = document.querySelector('#anoNascimento')
     const resultado = document.querySelector('#resultado')
 
-    if (anoNascimento.value.length === 0 || anoNascimento > anoAtual) return window.alert('Verifique os dados e tente novamente!')
+    if (anoNascimento.value.length === 0 || anoNascimento.value > anoAtual) return window.alert('Verifique os dados e tente novamente!')
     else {
-        const gêneroForm = document.querySelector('radsex')
-        const idade = anoAtual - anoNascimento
-        const sexo = ''
-        const img = document.createElement('img')
-        if (gêneroForm[0].checked) {
+        let generoForm = document.getElementsByName('radsex')
+        let idade = anoAtual - Number(anoNascimento.value)
+        let sexo = ''
+        let img = document.createElement('img')
+        img.setAttribute('id','foto')
+
+        if (generoForm[0].checked) {
             sexo = 'homem'
             if (idade >= 0 && idade <= 12) {
-                img.setAttribute('src' , menino.jpg)
-            } else if (idade >= 13 && idade <= 17)
-                img.setAttribute('src' , garoto.jpg) 
-            } else if (idade >= 18 && idade <= 59) {
-                img.setAttribute('src' , homem.jpg)
+                img.setAttribute('src' , 'menino.jpg')
+            } else if (idade < 17) {
+                img.setAttribute('src' , 'garoto.jpg') 
+            } else if (idade < 50) {
+                img.setAttribute('src' , 'homem.jpg')
             }  else { 
-                img.setAttribute('src' , idoso.jpg)
+                img.setAttribute('src' , 'idoso.jpg')
+            }
+        
+        } else if (generoForm[1].checked) {
+            sexo = 'mulher'
+            if (idade >= 0 && idade <= 12) {
+                img.setAttribute('src' , 'menina.jpg')
+            } else if (idade < 17) {
+                img.setAttribute('src' , 'garota.jpg') 
+            } else if (idade < 50) {
+                img.setAttribute('src' , 'mulher.jpg')
+            }  else { 
+                img.setAttribute('src' , 'idosa.jpg')
+        }
     }
-
-    resultado.style.textAlign = 'center'
-    resultado.innerHTML = (`Detectamos um ${sexo} com ${idade} anos.`)
-    resultado.appendChild(img)
-};
+        resultado.style.textAlign = 'center'
+        resultado.innerHTML = (`Detectamos ${sexo} com ${idade} anos.`)
+        resultado.appendChild(img)
+    }   
+}
