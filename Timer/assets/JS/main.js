74002fbdbd465c3934a57560d996afe9166df848
Hashimoto 1312 <kendi.campos@hotmail.com>
2021-10-20 09:52:29 -0300

    Finalizdo
    Terminei o Timer

--- Timer/assets/JS/main.js
+++ Timer/assets/JS/main.js
@@ -1,35 +1,44 @@
-function getTimeFromSeconds(segundos) {
-    const data = new Date(segundos * 1000);
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'UTC'
+function relogio() {
+    function getTimeFromSeconds(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        });
+    }
+    
+    const relogio = document.querySelector('.relogio');
+    let segundos = 0;
+    let timer;
+    
+    function iniciaRelogio() {
+        timer = setInterval(function() {
+           segundos++;
+           relogio.innerHTML = getTimeFromSeconds(segundos);
+       }, 1000);
+    };
+    
+    document.addEventListener('click', function(e) {
+        const elemento = e.target;
+        
+        if (elemento.classList.contains('zerar')) {
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausado' , 'iniciado')
+            segundos = 0;
+        }
+    
+        if (elemento.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado');
+            relogio.classList.add('iniciado');
+            clearInterval(timer);
+            iniciaRelogio();
+        }
+    
+        if (elemento.classList.contains('pausar')) {
+            clearInterval(timer);
+            relogio.classList.add('pausado');
+        }
     });
 }
-
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
-let segundos = 0;
-let timer;
-
-function iniciaRelogio() {
-    const timer = setInterval(function() {
-        segundos++;
-        relogio.innerHTML = getTimeFromSeconds(segundos);
-    }, 1000);
-}
-
-iniciar.addEventListener('click', function(event) {
-    clearInterval(timer);
-    iniciaRelogio();
-});
-
-pausar.addEventListener('click', function(event) {
-    clearInterval(timer);
-});
-
-zerar.addEventListener('click', function(event) {
-    clearInterval(timer);
-    relogio.innerHTML = '00:00:00';
-});
+relogio();
