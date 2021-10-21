8b24fdd46174954081f2eb35715ded4786c7b310
Hashimoto 1312 <kendi.campos@hotmail.com>
2021-10-21 10:12:10 -0300

    Quase terminando

---
+++ Tabuada/assets/JS/main.js
@@ -0,0 +1,23 @@
+function tabuada() {
+
+    const num = document.querySelector('#inputTabuada');
+    const tab = document.querySelector('.tabuada');
+    
+    if (num.value.length == 0) {
+        window.alert('Digite um número e tente novamente.'); 
+    } else {
+        const numero = Number(num.value);
+        let contador = 1;
+        tab.innerHTML = '';
+
+    repetirTabuada = () => {
+        while (c <= 10) {
+            let item = document.createElement('option');
+            item.text = `${numero} x ${contador} = ${numero * contador}`;
+            item.value = `tab${contador}`;
+            tab.appendChild(item);
+            contador++;
+            }         
+        }
+    }   
+}
