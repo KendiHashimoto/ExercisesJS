4134bd79caf2bd5a5103bfe30d3f8cbba89a54cb
Hashimoto 1312 <kendi.campos@hotmail.com>
2021-10-21 16:45:57 -0300

    Otimizei
    Diminui o numero de linhas

--- Tabuada/assets/JS/main.js
+++ Tabuada/assets/JS/main.js
@@ -1,15 +1,12 @@
 function tabuada() {
-
     const num = document.querySelector('#inputTabuada');
     const tab = document.querySelector('#lugarTabuada');
-
     if (num.value.length == 0) {
         window.alert('Digite um número e tente novamente.'); 
     } else {
         const numero = Number(num.value);
         let contador = 1;
         tab.innerHTML = '';
-
         while (contador < 11) {
             let item = document.createElement('option');
             item.text = `${numero} x ${contador} = ${numero * contador}`;
